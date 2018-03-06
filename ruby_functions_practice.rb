796a54589e2515e1e490030eaffd035e440cc8e6
Jaime Lopez <detec.azul@gmail.com>
2018-03-06 16:41:55 +0000

    Refactors Month Case Statement onto a Month array

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -31,62 +31,13 @@ def add_string_as_number(firststring, secondstring)
 end
 
 def number_to_full_month_name(number)
-  case number
-  when 1
-    return "January"
-  when 2
-    return "February"
-  when 3
-    return "March"
-  when 4
-    return "April"
-  when 5
-    return "May"
-  when 6
-    return "June"
-  when 7
-    return "July"
-  when 8
-    return "August"
-  when 9
-    return "September"
-  when 10
-    return "October"
-  when 11
-    return "November"
-  when 12
-    return "December"
-  end
-
+  month_array = [nil, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  return month_array[number]
 end
 
 def number_to_short_month_name(number)
-  case number
-  when 1
-    return "Jan"
-  when 2
-    return "Feb"
-  when 3
-    return "Mar"
-  when 4
-    return "Apr"
-  when 5
-    return "May"
-  when 6
-    return "Jun"
-  when 7
-    return "Jul"
-  when 8
-    return "Aug"
-  when 9
-    return "Sep"
-  when 10
-    return "Oct"
-  when 11
-    return "Nov"
-  when 12
-    return "Dec"
-  end
+  month_array = [nil, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  return month_array[number]
 end
 
 def volume_of_cube(side)
@@ -99,4 +50,4 @@ end
 
 def fahrenheit_to_celsius(fahrenheit)
   return fahrenheit - 17.22
-end 
+end
